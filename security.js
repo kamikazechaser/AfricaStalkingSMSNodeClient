0f3ba5d3af5df006fef629a9373087ce2f747989
BransonGitomeh <sirbranson@outlook.com>
2016-10-27 10:03:44 +0300

    testme

--- security.js
+++ security.js
@@ -9,10 +9,16 @@ app.get("/", (req, res) => {
     res.send("hello world!")
 })
 
-http.createServer(function(req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(8000);
+// http.createServer(function(req, res) {
+//     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//     res.end();
+// }).listen(8000);
+
+const port = 8000
+
+app.listen(port, (err) => {
+    console.log("server started on " + port)
+})
 
 https.createServer({
     key: fs.readFileSync("/etc/letsencrypt/archive/sabek.co.ke/privkey1.pem"),
