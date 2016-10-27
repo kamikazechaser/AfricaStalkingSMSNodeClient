d26b38507501eff79424ef77f6ea07ea5777edb6
BransonGitomeh <sirbranson@outlook.com>
2016-10-27 09:46:36 +0300

    back fs

--- security.js
+++ security.js
@@ -1,10 +1,11 @@
 var https = require('https')
 var http = require('http')
+var fs = require('fs')
 
 http.createServer(function(req, res) {
     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
-}).listen(80);
+}).listen(8000);
 
 https.createServer({
     key: fs.readFileSync("/etc/letsencrypt/archive/sabek.co.ke/privkey.pem"),
